4b4511b1b11fb4df51ac69125aa78692413cac62
takagi-souma <tskr.0310@icloud.com>
2021-03-05 10:52:29 +0900

    ユーザー管理機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  full_width = /\A[ぁ-んァ-ン一-龥]/
+  katakana = /\A[ァ-ヶー－]+\z/
+  validates :nickname, presence: true
+  validates :last_name,presence: true,format: {with: full_width , message: "Full-width characters"}
+  validates :last_name_kana, presence: true,format: {with: katakana , message: "Full-width katakana characters"}
+  validates :first_name, presence: true,format: {with: full_width , message: "Full-width characters"}
+  validates :first_name_kana, presence: true,format: {with: katakana , message: "Full-width katakana characters"}
+  validates :birth_date, presence: true
+  validates :password, format: {with: /\A[a-zA-Z0-9]+\z/ , message: "Password Include both letters and numbers"}
+
+  has_many :items
+  has_many :orders
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,95 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe '#create' do
+    it '全ての項目の入力が存在すれば登録できること' do
+      user = FactoryBot.build(:user)
+      expect(user).to be_valid
+    end
+
+    it 'nicknameがない場合は登録できないこと' do
+      user = FactoryBot.build(:user, nickname: nil)
+      user.valid?
+      expect(user.errors[:nickname]).to include("can't be blank")
+    end
+
+    it "emailがない場合は登録できないこと" do
+      user = FactoryBot.build(:user, email: nil)
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+
+    it "passwordがない場合は登録できないこと" do
+      user = FactoryBot.build(:user, password: nil)
+      user.valid?
+      expect(user.errors[:password]).to include("can't be blank")
+    end
+
+    it "last_nameがない場合は登録できないこと" do
+      user = FactoryBot.build(:user, last_name: nil)
+      user.valid?
+      expect(user.errors[:last_name]).to include("can't be blank")
+    end
+
+    it "last_name_kanaがない場合は登録できないこと" do
+      user = FactoryBot.build(:user, last_name_kana: nil)
+      user.valid?
+      expect(user.errors[:last_name_kana]).to include("can't be blank")
+    end
+
+    it "first_nameがない場合は登録できないこと" do
+      user = FactoryBot.build(:user, first_name: nil)
+      user.valid?
+      expect(user.errors[:first_name]).to include("can't be blank")
+    end
+
+    it "first_name_kanaがない場合は登録できないこと" do
+      user = FactoryBot.build(:user, first_name_kana: nil)
+      user.valid?
+      expect(user.errors[:first_name_kana]).to include("can't be blank")
+    end
+
+    it "birth_dateがない場合は登録できないこと" do
+      user = FactoryBot.build(:user, birth_date: nil)
+      user.valid?
+      expect(user.errors[:birth_date]).to include("can't be blank")
+    end
+
+    it "重複したemailが存在する場合登録できないこと" do
+      user = FactoryBot.create(:user) 
+      another_user = FactoryBot.build(:user, email: user.email) 
+      another_user.valid? 
+      expect(another_user.errors[:email]).to include("has already been taken")
+    end
+  
+    it "passwordが6文字以下であれば登録できないこと" do
+      user = FactoryBot.build(:user, password: "123ab", password: "123ab") 
+      user.valid?
+      expect(user.errors[:password]).to include("is too short (minimum is 6 characters)")
+    end
+
+    it 'last_nameが全角入力でなければ登録できないこと' do
+      user = FactoryBot.build(:user, last_name: "ｱｲｳｴｵ") 
+      user.valid?
+      expect(user.errors[:last_name]).to include("Full-width characters")
+    end
+
+    it 'first_nameが全角入力でなければ登録できないこと' do
+      user = FactoryBot.build(:user, first_name: "ｱｲｳｴｵ") 
+      user.valid?
+      expect(user.errors[:first_name]).to include("Full-width characters")
+    end
+
+    it 'last_name_kanaが全角カタカナでなければ登録できないこと' do
+      user = FactoryBot.build(:user, last_name_kana: "あいうえお") 
+      user.valid?
+      expect(user.errors[:last_name_kana]).to include("Full-width katakana characters")
+    end
+
+    it 'first_name_kanaが全角カタカナでなければ登録できないこと' do
+      user = FactoryBot.build(:user, first_name_kana: "あいうえお") 
+      user.valid?
+      expect(user.errors[:first_name_kana]).to include("Full-width katakana characters")
+    end
+  end
+end
