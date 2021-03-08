72b46bf05e32a805481a639f71949358427b64a3
takagi-souma <tskr.0310@icloud.com>
2021-03-08 11:27:32 +0900

    ユーザー管理機能修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,28 +1,6 @@
 class ItemsController < ApplicationController
+
   def index
-    @items = Item.all
+  @items = Item.all
   end
-
-  def new
-    @item = Item,new
-  end
-
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to controller: :items, action: :index
-    else
-      render "new"
-    end 
-  end
-
-
-  def destroy
-    if @item.destroy
-      redirect_to root_path
-    else
-      redirect_to root_path
-    end
-  end
-
 end
