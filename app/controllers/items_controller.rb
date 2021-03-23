4323b57e99c50faee973a3ae3f5f538f854bfb6f
takagi-souma <tskr.0310@icloud.com>
2021-03-23 10:11:36 +0900

    商品情報編集機能修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, except: [:index, :new, :create]
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :editm, :update]
   before_action :contributor_confirmation, only: [:edit, :update]
 
   def index
