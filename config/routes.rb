88efc45972ebe92d4aea6110c743e44dc1c5257b
takagi-souma <tskr.0310@icloud.com>
2021-03-23 18:01:38 +0900

    商品削除機能修正２

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, [:index, :create, :new, :show, :edit, :update, :destroy]
+  resources :items
 end
