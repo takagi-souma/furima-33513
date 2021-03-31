54e461bbc0eeb29fcafaf69e15a66a3a70270feb
takagi-souma <tskr.0310@icloud.com>
2021-03-31 17:41:43 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -33,12 +33,7 @@ ActiveRecord::Schema.define(version: 2021_03_26_061813) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "furimas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.integer "price", null: false
     t.integer "category_id", null: false
@@ -80,7 +75,7 @@ ActiveRecord::Schema.define(version: 2021_03_26_061813) do
     t.index ["order_id"], name: "index_shipping_addresses_on_order_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "nickname", null: false
@@ -94,6 +89,8 @@ ActiveRecord::Schema.define(version: 2021_03_26_061813) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
